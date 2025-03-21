14602d832d2abcd8b2daac40bd671158d9617789
Anthony Leon <anthonyfabian22lldev@gmail.com>
2025-03-21 11:11:32 +0100

    refactor: some adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import {configDotenv} from "dotenv";
+
+configDotenv()
 
 // https://astro.build/config
 export default defineConfig({
